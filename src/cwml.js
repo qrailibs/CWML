69d9465d353cb7222bfc1db08e3b6f8ccc614a47
qrai <38942869+qrai@users.noreply.github.com>
2021-02-06 15:07:22 +0400

    Update cwml.js

--- src/cwml.js
+++ src/cwml.js
@@ -1,121 +1,56 @@
-//-----------------------
-// API
-//-----------------------
 const cwml = {
-    // Tags...
-    tags: [],
+    tags: {},
     registerTag: function(_tag, _descriptor = new CwmlTagDescriptor()) {
         window.customElements.define(_tag.toLowerCase(), 
             class CwmlTag extends HTMLElement {
-                tag = '';
                 descriptor = new CwmlTagDescriptor();
 
                 constructor(){
                     super(); 
 
-                    this.tag = _tag;
                     this.descriptor = _descriptor;
 
                     // Add observed events
                     for(var event_name in this.descriptor.eventsObserved) {
                         var event_func = this.descriptor.eventsObserved[event_name];
-
-                        //Default listeners
                         if(!event_name.startsWith('__')) {
-                            this.addEventListener(event_name, event_func);
+                            this.addEventListener(event_name, function(e) { event_func(e.target); } );
                         }
                     }
                 }
 
-                // Common events
-                connectedCallback() {
-                    //If event callback defined
-                    if(this.descriptor.eventsObserved['__added__'] !== undefined) {
-                        this.descriptor.eventsObserved['__added__'](this);
-                    }
-                }
-                disconnectedCallback() {
-                    //If event callback defined
-                    if(this.descriptor.eventsObserved['__removed__'] !== undefined) {
-                        this.descriptor.eventsObserved['__removed__'](this);
-                    }
-                }
-                adoptedCallback() {
-                    //If event callback defined
-                    if(this.descriptor.eventsObserved['__adopted__'] !== undefined) {
-                        this.descriptor.eventsObserved['__adopted__'](this);
-                    }
-                }
+                connectedCallback() { this.descriptor.eventsObserved['__added__'] !== undefined ? this.descriptor.eventsObserved['__added__'](this) : undefined; }
+                disconnectedCallback() { this.descriptor.eventsObserved['__removed__'] !== undefined ? this.descriptor.eventsObserved['__removed__'](this) : undefined; }
+                adoptedCallback() { this.descriptor.eventsObserved['__adopted__'] !== undefined ? this.descriptor.eventsObserved['__adopted__'](this) : undefined; }
 
+                static get observedAttributes() {
+                    return Object.keys(_descriptor.attrsObserved);
+                }
                 attributeChangedCallback(attrName, oldVal, newVal) {
-                    
+                    this.descriptor.attrsObserved[attrName] !== undefined ? this.descriptor.attrsObserved[attrName](this, oldVal, newVal) : undefined;
                 }
             }
         );
-        this.tags.push(_tag)
+
+        this.tags[_tag] = {
+            events: Object.keys(_descriptor.eventsObserved),
+            attributes: Object.keys(_descriptor.attrsObserved),
+        }
     },
+
     isTagSupported: function(tag) {
-        return tags[tag] !== undefined;
+        return this.tags[tag] !== undefined;
     },
-
-    // Logging...
-    log: [],
-    writeLog: function(type,message) {
-        this.log.push({
-            'type': type,
-            'message':message
-        });
-    },
-    writeInfo: function(text) {
-        console.log('CWML Info:' + text);
-        this.writeLog('info', text);
-    },
-    writeWarn: function(text) {
-        console.warn('CWML Warning:' + text);
-        this.writeLog('warning', text);
-    },
-    writeError: function(text) {
-        console.error('CWML Error:' + text);
-        this.writeLog('error', text);
-    },
-
-    // Testing...
-
+    isAttrSupported: function(tag,attr) {
+        return this.tags[tag] !== undefined ? this.tags[tag].attributes.includes(attr) : false;
+    }
 }
 
-//-----------------------
-// Classes
-//-----------------------
 class CwmlTagDescriptor {
+    eventsObserved = {};
+    attrsObserved = {};
     constructor(events={}, attrs={}) {
         this.eventsObserved = events;
         this.attrsObserved = attrs;
     }
-
-    // Events...
-    eventsObserved = {};
-    whenEvent(event, func) {
-        this.eventsObserved[event] = func;
-    }
-    whenClick(func) {
-        this.whenEvent('click', func);
-    }
-    whenAdded(func) {
-        this.whenEvent('__added__', func);
-    }
-    whenRemoved(func) {
-        this.whenEvent('__removed__', func);
-    }
-    whenAdopted(func) {
-        this.whenEvent('__adopted__', func);
-    }
-
-    // Attributes...
-    attrsObserved = {};
-    registerAttr(attr, whenChanged=function(oldVal,newVal){}) {
-        this.attrsObserved[attr] = whenChanged;
-    }
-
-    // Content...
-    //TODO
 }
